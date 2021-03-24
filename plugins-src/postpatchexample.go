a6db25d8e11a753151a4ea969ad0f757cd460bad
Wade Mealing <wmealing@redhat.com>
2021-03-24 14:22:01 +1000

    Silly things.

--- plugins-src/postpatchexample.go
+++ plugins-src/postpatchexample.go
@@ -1,11 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/sourcegraph/go-diff/diff"
+)
 
 type greeting string
 
-func (g greeting) Greet() {
+func (g greeting) Greet(d diff.FileDiff) {
 	fmt.Println("Hello Universe")
+	printed, err := diff.PrintFileDiff(&d)
+
+	if err != nil {
+		fmt.Println("wat")
+
+	}
+	if _, err := os.Stdout.Write(printed); err != nil {
+		log.Fatal(err)
+	}
+
 }
 
 // exported
